fb03d311438b6f9010b25c6a2b484b323e04cdaf
Staford <xx_staford_xx@hotmail.com>
2026-01-09 17:23:18 +0100

    Create TotsALaDreta.java

---
+++ Llistes/TotsALaDreta.java
@@ -0,0 +1,22 @@
+package Llistes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class TotsALaDreta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<Integer> lista = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			lista.add(sc.nextInt());
+		}
+		Collections.rotate(lista, 1);
+		for (int num = lista; num < lista.size();) {
+			System.out.print(lista + " ");
+		}
+	}
+}
